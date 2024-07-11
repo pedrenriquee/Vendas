c1d023e7ffed08ae3adfb32207e0eb2e353e28cb
Pedro Henrique <pedrollopes31@gmail.com>
2024-07-10 23:34:53 -0300

    meu primeiro commit

---
+++ src/main/java/org/example/VendasApplication.java
@@ -0,0 +1,26 @@
+package org.example;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class VendasApplication {
+
+    @Autowired
+    @Qualifier("applicationName")
+    private String ApplicationName;
+
+    @GetMapping("/hello")
+    public String helloWorld(){
+        return ApplicationName ;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(VendasApplication.class, args);
+    }
+}
