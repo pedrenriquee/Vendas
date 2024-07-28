f418b3464c590a88ba7cb18778e165f075e1cd05
Pedro Henrique <pedrollopes31@gmail.com>
2024-07-28 17:25:21 -0300

    finalizacao do crud

--- src/main/java/org/example/repositoy/Clientes.java
+++ src/main/java/org/example/repositoy/Clientes.java
@@ -16,23 +16,49 @@ public class Clientes {
 
    private static String INSERT = "insert into CLIENTE (nome) values (?)";
    private static String SELECT_ALL = "SELECT * FROM CLIENTE";
+   private static String UPDATE = "update CLIENTE set nome = ? where id = ?";
+   private static String DELETE = "delete from CLIENTE where id = ?";
+
   @Autowired
   private  JdbcTemplate jdbcTemplate;
-  public CLIENTE salvar(CLIENTE cliente){
+  public CLIENTE inserir(CLIENTE cliente){
       jdbcTemplate.update(INSERT, new Object[]{cliente.getNome()});
       return cliente;
   }
 
-  public List<CLIENTE> obterTodos(){
-      return jdbcTemplate.query(SELECT_ALL, new RowMapper<CLIENTE>() {
-          @Override
-          public CLIENTE mapRow(ResultSet rs, int rowNum) throws SQLException {
-              Long id = rs.getLong("id");
-              String nome = rs.getString("nome");
-              return new CLIENTE(nome, id);
-          }
-      });
+  public CLIENTE atualizar(CLIENTE cliente){
+      jdbcTemplate.update(UPDATE, new Object[]{cliente.getNome(), cliente.getId()} );
+      return cliente;
   }
+
+  public void deletar(CLIENTE cliente){
+      deletar(cliente.getId());
+  }
+
+  public void deletar( Long id){
+      jdbcTemplate.update(DELETE, new Object[]{id});
+  }
+
+  public List<CLIENTE> buscarPorNome(String nome){
+      return  jdbcTemplate.query(SELECT_ALL.concat(
+              "where nome like ?"), new Object[]{"%"+ nome + "%"}, obterCLIENTEMapper());
+  }
+
+
+  public List<CLIENTE> obterTodos(){
+      return jdbcTemplate.query(SELECT_ALL, obterCLIENTEMapper());
+  }
+
+    private static RowMapper<CLIENTE> obterCLIENTEMapper() {
+        return new RowMapper<CLIENTE>() {
+            @Override
+            public CLIENTE mapRow(ResultSet resultSet, int i) throws SQLException {
+                Long id = resultSet.getLong("id");
+                String nome = resultSet.getString("nome");
+                return new CLIENTE(id, nome);
+            }
+        };
+    }
 
 
 }
