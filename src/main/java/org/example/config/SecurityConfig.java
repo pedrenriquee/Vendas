76fd756c48d75b315be7a0b6a10713616641a2e7
Pedro Henrique <pedrollopes31@gmail.com>
2024-10-20 15:54:00 -0300

    adicionando modulo de seguranca e configs iniciais

---
+++ src/main/java/org/example/config/SecurityConfig.java
@@ -0,0 +1,14 @@
+package org.example.config;
+
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfiguration {
+
+ public PasswordEncoder passwordEncoder(){
+     return new BCryptPasswordEncoder();
+ }
+}
