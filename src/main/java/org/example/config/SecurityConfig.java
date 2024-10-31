2dafe7fe450ca7dbe72de5a39ff444d11d6e3827
Pedro Henrique <pedrollopes31@gmail.com>
2024-10-31 17:19:04 -0300

    adicionando config de autorização

--- src/main/java/org/example/config/SecurityConfig.java
+++ src/main/java/org/example/config/SecurityConfig.java
@@ -1,9 +1,12 @@
 package org.example.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfiguration {
@@ -11,4 +14,15 @@ public class SecurityConfig extends WebSecurityConfiguration {
  public PasswordEncoder passwordEncoder(){
      return new BCryptPasswordEncoder();
  }
+
+ @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+     http
+             .csrf(csrf -> csrf.disable())
+             .authorizeHttpRequests(authorize -> authorize
+             .requestMatchers("/api/clientes/**").permitAll());
+    return http.build();
+ }
+
+
 }
